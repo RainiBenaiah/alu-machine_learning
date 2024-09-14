5a28fb35c2b3caa50f02ae04facc25bbbd56b3f1
RainiBenaiah <b.raini@alustudent.com>
2024-09-14 13:49:43 -0700

    last

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
-"""defines a fuction that multiplies two matrices"""
+"""Defines a function that multiplies two matrices."""
 
-
+import numpy as np
 
 def mat_mul(mat1, mat2):
-    """returns new matrix that is the product of two 2D matrice"""
+    """Returns a new matrix that is the product of two 2D matrices."""
+
     if len(mat1[0]) != len(mat2):
-       return None
+        return None
 
-    newmat = [[0 for i in range(len(mat2[0]))] for j in range(len(mat1))]
-
-    for i in range(len(mat1)):
-        for j in range(len(mat2[0])):
-            for k in range(len(mat2)):
-                newmat[i][j] += mat1[i][k] * mat2[k][j]
-    return newmat
+    return np.dot(mat1, mat2)
