8273fd6b44adc0d1e2f2fd43ea3781c46f58b280
RainiBenaiah <b.raini@alustudent.com>
2024-09-21 14:07:02 -0700

    definite

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python3
-
 """
 Defines a function that Calculates the definiteness of a matrix.
 """
+
 
 import numpy as np
 
 
 def definiteness(matrix):
     """
-    matrix: A numpy.ndarray of shape (n, n) whose definiteness
-
-    Returns: matrix does not fit any of the definiteness categories,
-
-    Raises:
-        TypeError: If matrix is not a numpy.ndarray.
+    matrix: A numpy.ndarray of shape (n, n) whose definitenes
     """
-
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
 
