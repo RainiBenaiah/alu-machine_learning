c154de476089c072f5a05f83254a7cd8f8b2258c
RainiBenaiah <b.raini@alustudent.com>
2024-10-05 02:06:41 -0700

    cor

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -9,6 +9,6 @@ def summation_i_squared(n):
     module uses squares formula to add squares upto n
     """
     if isinstance(n, int) and n >= 1:
-        return (n * (n + 1) * (2 * n + 1)) //6
+        return (n * (n + 1) * (2 * n + 1))//6
     else:
         return None
