713028544b6cabb0d5d07c872124d83744e19180
RainiBenaiah <b.raini@alustudent.com>
2024-10-05 01:55:03 -0700

    add

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,7 +6,7 @@ defines a function that adds squares from i to n
 
 def summation_i_squared(n):
     """
-    The moduleuses squares formula to add squares upto n
+    module uses squares formula to add squares upto n
     """
     if isinstance(n, int) and n >= 1:
         return (n * (n + 1) * (2 * n + 1))
