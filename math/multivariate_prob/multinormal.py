21a488d33a17ba31de5f6356f64569884f6dec0e
RainiBenaiah <b.raini@alustudent.com>
2024-10-14 10:13:18 -0700

    debug multinormal

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -17,6 +17,8 @@ class MultiNormal:
             raise TypeError("data must be a 2D numpy.ndarray")
         if data.shape[0] < 2:
             raise ValueError("data must contain multiple data points")
+        if data.shape[1] < 2:
+            raise ValueError("data must contain multiple data points (columns)")
         self.mean = np.mean(data, axis=1).reshape(-1, 1)
         centered_data = data - self.mean
         self.cov = np.dot(centered_data, centered_data.T) / (data.shape[1] - 1)
