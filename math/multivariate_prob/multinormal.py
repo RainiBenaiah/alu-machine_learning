28dd3a46e6a75e5b50fe1f3d6fb4ae0d30c332f1
RainiBenaiah <b.raini@alustudent.com>
2024-10-14 10:01:29 -0700

    debug multinormal

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 '''
-class for multinormal distribution
+Class for multinormal distribution
 '''
 import numpy as np
 
 
 class MultiNormal:
-      """
-      Multinormal class that represents a multinormal dist
-      """
-      def __init__(self, data):
-          """
-          Constructor
-          """
-          if not isinstance(data, np.ndarray) or len(data.shape) != 2:
-              raise TypeError("data must be a 2D numpy.ndarray")
-          if data.shape[0] < 2:
-              raise ValueError("data must contain multiple data points")
-          self.mean = np.mean(data, axis=1).reshape(-1, 1)
-          self.cov = np.dot(data - self.mean, data.T) / (data.shape[1] - 1)
+    """
+    MultiNormal class that represents a multinormal distribution.
+    """
+    def __init__(self, data):
+        """
+        Constructor
+        """
+        if not isinstance(data, np.ndarray) or len(data.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        if data.shape[0] < 2:
+            raise ValueError("data must contain multiple data points")
+        self.mean = np.mean(data, axis=1).reshape(-1, 1)
+        centered_data = data - self.mean
+        self.cov = np.dot(centered_data, centered_data.T) / (data.shape[1] - 1)
