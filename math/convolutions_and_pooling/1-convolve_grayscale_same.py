6cb8c5342de25af02ea30c8cfa1ff6900e041d3c
RainiBenaiah <b.raini@alustudent.com>
2024-10-24 16:29:57 -0700

    con 1

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,31 +1,35 @@
 #!/usr/bin/env python3
-'''
-defines a same convolution padding function
-'''
+"""
+defines a function that performs a same convolution
+"""
 import numpy as np
 
 
-def convolve_grayscale_padding(images, kernel, padding):
-    """
+def convolve_grayscale_same(images, kernel):
+    '''
     Function to perform a same convolution on grayscale images
-    Returns:numpy.ndarray - shape (m, h, w)
-    """
+    '''
+
     m, h, w = images.shape
     kh, kw = kernel.shape
-    ph, pw = padding
-
-    output = np.zeros((m, h, w))
+  
+    pad_h = kh // 2
+    pad_w = kw // 2
 
     padded_images = np.pad(
-        images,
-        ((0, 0), (ph, ph), (pw, pw))
-        )
+            images,
+            ((0, 0),(pad_h, pad_h),(pad_w, pad_w)),
+            mode='constant'
+            )
+
+    convolved_images = np.zeros((m, h, w))
 
     for i in range(h):
-        for j in range(w):
-            output[:, i, j] = np.sum(
-                padded_images[:, i:i+kh, j:j+kw] * kernel,
-                axis=(1, 2)
-                )
-
-    return output
+     for j in range(w):
+        region = padded_images[:, i:i+kh, j:j+kw]
+        convolved_images[:, i, j] = np.sum(
+           region * kernel,
+           axis=(1, 2)
+           )
+  
+    return convolved_images
