c36ac989fdecbefa0b162566e68d77ab6adb27b6
RainiBenaiah <b.raini@alustudent.com>
2024-10-24 16:47:11 -0700

    pyco

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -12,24 +12,24 @@ def convolve_grayscale_same(images, kernel):
 
     m, h, w = images.shape
     kh, kw = kernel.shape
-  
+
     pad_h = kh // 2
     pad_w = kw // 2
 
     padded_images = np.pad(
             images,
-            ((0, 0),(pad_h, pad_h),(pad_w, pad_w)),
+            ((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
             mode='constant'
             )
 
     convolved_images = np.zeros((m, h, w))
 
     for i in range(h):
-     for j in range(w):
-        region = padded_images[:, i:i+kh, j:j+kw]
-        convolved_images[:, i, j] = np.sum(
-           region * kernel,
-           axis=(1, 2)
-           )
-  
+        for j in range(w):
+            region = padded_images[:, i:i+kh, j:j+kw]
+            convolved_images[:, i, j] = np.sum(
+                region * kernel,
+                axis=(1, 2)
+                )
+
     return convolved_images
