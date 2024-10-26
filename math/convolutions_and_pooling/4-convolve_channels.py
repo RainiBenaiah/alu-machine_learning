d72c627a1b3569dfe472248b5674277d374ef41e
RainiBenaiah <127124519+RainiBenaiah@users.noreply.github.com>
2024-10-26 10:29:41 +0200

    Update 4-convolve_channels.py

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -42,7 +42,7 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
             horiz_end = horiz_start + kw
 
             slice_img = padded_images[
-            :, vert_start:vert_end, horiz_start:horiz_end, :
+                :, vert_start:vert_end, horiz_start:horiz_end, :
             ]
             convolved[:, i, j] = np.sum(slice_img * kernel, axis=(1, 2, 3))
 
