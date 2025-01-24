cbfc26d6d271d94eb90a1904bf18d1b74f36c23f
RainiBenaiah <127124519+RainiBenaiah@users.noreply.github.com>
2025-01-24 16:04:21 +0200

    Update 16-deep_neural_network.py

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -30,12 +30,13 @@ class DeepNeuralNetwork:
 
         for i in range(self.L):
             if i == 0:
-                self.weights['W1'] = np.random.randn(layers[i], nx)
-                * np.sqrt(2 / nx)
+                self.weights['W1'] = np.random.randn(
+                    layers[i], nx) * np.sqrt(2 / nx)
                 self.weights['b1'] = np.zeros((layers[i], 1))
 
             else:
-                self.weights['W{}'.format(i + 1)] = np.random.randn(layers[i], layers[i - 1])
-                * np.sqrt(2 / layers[i - 1])
+                self.weights['W{}'.format(
+                    i + 1)] = np.random.randn(layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
 
-                self.weights['b{}'.format(i + 1)] = np.zeros((layers[i], 1))
+                self.weights['b{}'.format(
+                    i + 1)] = np.zeros((layers[i], 1))
