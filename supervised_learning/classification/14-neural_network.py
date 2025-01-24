55c2438b99cf72d50f368b19350b77d0192e23be
RainiBenaiah <127124519+RainiBenaiah@users.noreply.github.com>
2025-01-24 15:47:57 +0200

    Update 14-neural_network.py

--- supervised_learning/classification/14-neural_network.py
+++ supervised_learning/classification/14-neural_network.py
@@ -1,4 +1,5 @@
-n Neural network"""
+#!/usr/bin/env python3
+"""Neural network"""
 
 
 import numpy as np
@@ -9,81 +10,83 @@ class NeuralNetwork:
     neural network with one hidden layer
     '''
     def __init__(self, nx, nodes):
-        '''
-        Neuron class constructor
-        '''
-        if not isinstance(nx, int):
+        """
+        Class constructor
+        """
+        if not type(nx) is int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(nodes, int):
+        if not type(nodes) is int:
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
-
-        self..__W1 = np.random.randn(nx, nodes)
+        self.__W1 = np.random.randn(nodes, nx)
         self.__b1 = np.zeros((nodes, 1))
         self.__A1 = 0
-        self.__W2 = np.random.randn(nodes, 1)
+        self.__W2 = np.random.randn(1, nodes)
         self.__b2 = 0
-        self.__A2  = 0
+        self.__A2 = 0
 
     @property
     def W1(self):
-        '''
-        getter function
-        '''
+        """
+        Getter function for W1
+        """
         return self.__W1
 
     @property
     def b1(self):
-        '''
-        getter function
-        '''
+        """
+        Getter function for b1
+        """
         return self.__b1
 
     @property
     def A1(self):
-        '''
-        getter function
-        '''
+        """
+        Getter function for A1
+        """
         return self.__A1
 
     @property
     def W2(self):
-        '''
-        getter function
-        '''
+        """
+        Getter function for W2
+        """
         return self.__W2
 
     @property
     def b2(self):
-        '''
-        getter function
-        '''
+        """
+        Getter function for b2
+        """
         return self.__b2
 
     @property
     def A2(self):
-        '''
-        getter function
-        '''
+        """
+        Getter function for A2
+        """
         return self.__A2
 
     def forward_prop(self, X):
         """
-        Forward propagation function
+        Calculates the forward propagation of the neural network
         """
-        self.__A1 = 1 / (1 + np.exp(-(np.dot(self.__W1.T, X) + self.__b1)))
-        self.__A2 = 1 / (1 + np.exp(-(np.dot(self.__W2.T, self.__A1) + self.__b2)))
+        Z1 = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+        Z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
         return self.__A1, self.__A2
 
     def cost(self, Y, A):
         """
-        Cost of the model
+        Calculates the cost of the model using logistic regression
         """
         m = Y.shape[1]
-        return -np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A))) / m
+        cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
 
     def evaluate(self, X, Y):
         """
@@ -99,14 +102,14 @@ class NeuralNetwork:
         """
         m = Y.shape[1]
         dz2 = A2 - Y
-        dw2 = np.matmul(A1, dz2.T) / m
-        db2 = np.sum(dz2, axis=1, keepdims=True) / m
-        dz1 = np.matmul(self.__W2, dz2) * (A1 * (1 - A1))
-        dw1 = np.matmul(X, dz1.T) / m
-        db1 = np.sum(dz1, axis=1, keepdims=True) / m
-        self.__W2 = self.__W2 - (alpha * dw2).T
+        dw2 = 1 / m * np.matmul(dz2, A1.T)
+        db2 = 1 / m * np.sum(dz2, axis=1, keepdims=True)
+        dz1 = np.matmul(self.__W2.T, dz2) * A1 * (1 - A1)
+        dw1 = 1 / m * np.matmul(dz1, X.T)
+        db1 = 1 / m * np.sum(dz1, axis=1, keepdims=True)
+        self.__W2 = self.__W2 - alpha * dw2
         self.__b2 = self.__b2 - alpha * db2
-        self.__W1 = self.__W1 - (alpha * dw1).T
+        self.__W1 = self.__W1 - alpha * dw1
         self.__b1 = self.__b1 - alpha * db1
 
     def train(self, X, Y, iterations=5000, alpha=0.05):
