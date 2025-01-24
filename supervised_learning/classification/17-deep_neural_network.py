e76b1559f66c779f72be6a92f7ee0db9bd988c3b
RainiBenaiah <127124519+RainiBenaiah@users.noreply.github.com>
2025-01-24 16:19:06 +0200

    Update 17-deep_neural_network.py

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -6,33 +6,44 @@ import numpy as np
 
 
 class DeepNeuralNetwork:
-    '''
-    Initialize Deep Neural Network class
-    '''
+    """Deep Neural Network"""
     def __init__(self, nx, layers):
-        '''
-        Deep Neural Network class constructor
-        '''
-        if not isinstance(nx, int):
+        """
+        nx: no of input features
+        layers: list repr no of nodes
+        L: no of layers in NN
+        cache: Dict with intermediary values
+        weights: Dict to hold weights n biases
+        """
+        if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list):
-            raise TypeError("layers must be a list of positive integers")
-        if len(layers) < 1:
-            raise TypeError("layers must be a list of positive integers")
-        if min(layers) < 1:
+        if (
+            type(layers) is not list
+            or len(layers) < 1
+            or min(layers) < 1
+        ):
             raise TypeError("layers must be a list of positive integers")
 
-        self.L = len(layers)
-        self.cache = {}
-        self.weights = {}
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
 
-        for i in range(self.L):
-            if i == 0:
-                self.weights['W1'] = np.random.randn(
-                    layers[i], nx) * np.sqrt(2 / nx)
-                self.weights['b1'] = np.zeros((layers[i], 1))
+        for layer in range(self.__L):
+            if layer == 0:
+                self.__weights['W1'] = np.random.randn(
+                    layers[0], nx) * np.sqrt(2 / nx)
+                self.__weights['b1'] = np.zeros([layers[0], 1])
+
+            else:
+                self.__weights['W{}'.format(layer+1)] = np.random.randn(
+                    layers[layer],
+                    layers[layer-1]) * np.sqrt(2. / layers[layer-1])
+
+                self.__weights['b{}'.format(
+                    layer+1)] = np.zeros((layers[layer], 1))
+
     @property
     def L(self):
         """Getter for L"""
