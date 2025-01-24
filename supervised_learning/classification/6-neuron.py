169a35df4ece918223918553eb4ee407369a6fab
RainiBenaiah <127124519+RainiBenaiah@users.noreply.github.com>
2025-01-24 13:02:47 +0200

    Update 6-neuron.py

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -57,7 +57,8 @@ class Neuron():
         Calculates the cost of the model using logistic regression
         """
         m = Y.shape[1]
-        cost = -np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A))) / m
+        cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) *
+                               np.log(1.0000001 - A))
         return cost
         
     def evaluate(self, X, Y):
@@ -73,26 +74,26 @@ class Neuron():
         Calculates one pass of gradient descent on the neuron
         """
         m = Y.shape[1]
-       dz = A - Y
-       db = np.sum(dz) / m
-       dw = np.matmul(X, dz.T) / m
-       self.__W = self.__W - (alpha * dw).T
-       self.__b = self.__b - alpha * db
-       return self.__W, self.__b
+        dz = A - Y
+        db = np.sum(dz) / m
+        dw = np.matmul(X, dz.T) / m
+        self.__W = self.__W - (alpha * dw).T
+        self.__b = self.__b - alpha * db
+        return self.__W, self.__b
 
-   def train(self, X, Y, iterations=5000, alpha=0.05):
-       """
-       Trains the neuron
-       """
-       if type(iterations) != int:
-           raise TypeError("iterations must be an integer")
-       if iterations < 1:
-           raise ValueError("iterations must be a positive integer")
-       if type(alpha) != float:
-           raise TypeError("alpha must be a float")
-       if alpha <= 0:
-          raise ValueError("alpha must be positive")
-       for i in range(iterations):
-           A = self.forward_prop(X)
-           self.gradient_descent(X, Y, A, alpha)
-       return self.evaluate(X, Y)
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """
+        Trains the neuron
+        """
+        if type(iterations) != int:
+            raise TypeError("iterations must be an integer")
+        if iterations < 1:
+            raise ValueError("iterations must be a positive integer")
+        if type(alpha) != float:
+            raise TypeError("alpha must be a float")
+        if alpha <= 0:
+           raise ValueError("alpha must be positive")
+        for i in range(iterations):
+            A = self.forward_prop(X)
+            self.gradient_descent(X, Y, A, alpha)
+        return self.evaluate(X, Y)
