72d529753c4088b0a70272b22bfbdb20d78e8775
RainiBenaiah <127124519+RainiBenaiah@users.noreply.github.com>
2025-01-24 15:13:56 +0200

    Update 9-neural_network.py

--- supervised_learning/classification/9-neural_network.py
+++ supervised_learning/classification/9-neural_network.py
@@ -10,9 +10,9 @@ class NeuralNetwork:
     neural network with one hidden layer
     '''
     def __init__(self, nx, nodes):
-        '''
-        Neuron class constructor
-        '''
+        """
+        Class constructor
+        """
         if not type(nx) is int:
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -21,52 +21,51 @@ class NeuralNetwork:
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
-
-        self.W1 = np.random.randn(nodes, nx)
-        self.b1 = np.zeros((nodes, 1))
-        self.A1 = 0
-        self.W2 = np.random.randn(1, nodes)
-        self.b2 = 0
-        self.A2 = 0
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
 
     @property
     def W1(self):
-        '''
-        getter function
-        '''
+        """
+        Getter function for W1
+        """
         return self.__W1
 
     @property
     def b1(self):
-        '''
-        getter function
-        '''
+        """
+        Getter function for b1
+        """
         return self.__b1
 
     @property
     def A1(self):
-        '''
-        getter function
-        '''
+        """
+        Getter function for A1
+        """
         return self.__A1
 
     @property
     def W2(self):
-        '''
-        getter function
-        '''
+        """
+        Getter function for W2
+        """
         return self.__W2
 
     @property
     def b2(self):
-        '''
-        getter function
-        '''
+        """
+        Getter function for b2
+        """
         return self.__b2
 
     @property
     def A2(self):
-        '''
-        getter function
-        '''
+        """
+        Getter function for A2
+        """
         return self.__A2
