aa28eaf2f943d7c8f9d7391f60e6ddccb323e5bb
RainiBenaiah <127124519+RainiBenaiah@users.noreply.github.com>
2025-01-24 16:00:58 +0200

    Update 16-deep_neural_network.py

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -23,18 +23,19 @@ class DeepNeuralNetwork:
             raise TypeError("layers must be a list of positive integers")
         if min(layers) < 1:
             raise TypeError("layers must be a list of positive integers")
-        
+
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
         for i in range(self.L):
             if i == 0:
-                self.weights['W1'] = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
+                self.weights['W1'] = np.random.randn(layers[i], nx)
+                * np.sqrt(2 / nx)
                 self.weights['b1'] = np.zeros((layers[i], 1))
 
             else:
-                self.weights['W{}'.format(i + 1)] = np.random.randn(layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+                self.weights['W{}'.format(i + 1)] = np.random.randn(layers[i], layers[i - 1])
+                * np.sqrt(2 / layers[i - 1])
 
                 self.weights['b{}'.format(i + 1)] = np.zeros((layers[i], 1))
-
