d27f956f225af50f5874c49d29d256a2b2e24969
RainiBenaiah <b.raini@alustudent.com>
2025-01-24 15:37:40 +0000

    encode

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
-"""one hot encode"""
+"""one hot encoding"""
+
+
 import numpy as np
 
 
 def one_hot_encode(Y, classes):
     """
-    One hot encode
+    Converts numerical label vectors
+    into one-hot matrix
+    Y-Contains numeric class labels
+    m - no of examples
+    classes- maximum no of classes
     """
-    if not isinstance(Y, np.ndarray) or len(Y.shape) == 1:
+    if not isinstance(Y, np.ndarray) or len(Y.shape) != 1:
         return None
-    if not isinstance(classes, int) or classes <= np.amax(Y):
+    if not isinstance(classes, int) or classes <= np.max(Y):
         return None
 
     m = Y.shape[0]
-    one_hot = np.zeros((classes, m))
+    one_hot_matrix = np.zeros((classes, m))
 
     for idx, val in enumerate(Y):
         if val >= classes or val < 0:
             return None
-        one_hot[val][idx] = 1
+        one_hot_matrix[val, idx] = 1
 
-    return one_hot
+    return one_hot_matrix
