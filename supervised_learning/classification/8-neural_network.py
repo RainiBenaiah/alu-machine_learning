71260e16981d787583274a280a7a52a410b187ec
RainiBenaiah <127124519+RainiBenaiah@users.noreply.github.com>
2025-01-24 14:58:22 +0200

    Update 8-neural_network.py

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -14,18 +14,18 @@ class NeuralNetwork:
         '''
         Neuron class constructor
         '''
-        if not isinstance(nx, int):
+       if not type(nx) is int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(nodes, int):
+        if not type(nodes) is int:
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
 
-        self.W1 = np.random.randn(nx, nodes)
+        self.W1 = np.random.randn(nodes, nx)
         self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
-        self.W2 = np.random.randn(nodes, 1)
+        self.W2 = np.random.randn(1, nodes)
         self.b2 = 0
         self.A2 = 0
